eb24fcd3f9d62d5e3949715a929c653f57e341f6
Maas Lalani <maaslalani1@gmail.com>
2020-01-13 10:03:31 -0500

    minutes < 10

--- util/time.js
+++ util/time.js
@@ -6,6 +6,9 @@ function convertTime(seconds) {
   minutes = Math.floor(seconds / 60)
   seconds = seconds % 60
 
+  if (minutes < 10)
+    minutes = `0${minutes}`
+
   if (seconds < 10)
     seconds = `0${seconds}`
 
